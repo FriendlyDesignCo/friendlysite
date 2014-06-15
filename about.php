f5ffcd3857a852825e53fc069442a4b866d8e909
Ross Nover <ross@friendlydesign.co>
2014-06-15 12:18:55 -0400

    Fixed it.

--- about.php
+++ about.php
@@ -7,12 +7,11 @@
     </div>
     <div class="abouttext">
         <p><span>Friendly Design Co.</span> solves creative problems with innovative solutions, builds brands and develops dynamic user experiences. Founded in 2012 by Ross Nover and Geoff Silverstein, FDC is a full-service design studio specializing in branding and interactive design and development. Ross and Geoff bring combined experience working for Fortune 500 companies, small businesses, and non-profits alike across an array of operational fields and around the United States. Friendly Design collaborates with talented developers, illustrators, and fellow designers at the top of their professional fields.</p>
-        <div class="about-social">
-            <a href="http://rosscott.tumblr.com/"><span class="fi-social-tumblr"></span></a>
-            <a href="http://rosscott.tumblr.com/"><span class="icon-tumblr"></span></a>
-            <a href="http://rosscott.tumblr.com/"><span class="icon-tumblr"></span></a>
-            <a href="http://rosscott.tumblr.com/"><span class="icon-tumblr"></span></a>
-            <a href="http://rosscott.tumblr.com/"><span class="icon-tumblr"></span></a>
+        <div class="about-social">       
+            <a href="mailto:someone@friendlydesign.co"><span class="icon-email"></span></a>
+            <a href="http://fyeah.friendlydesign.co/"><span class="icon-tumblr"></span></a>
+            <a href="http://www.twitter.com/friendlydc"><span class="icon-twitter"></span></a>
+            <a href="http://www.linkedin.com/company/friendly-design-co"><span class="icon-linkedin"></span></a>
         </div>
     </div>
 </div>
@@ -23,11 +22,11 @@
     <div class="abouttext">
         <p><span>Ross</span> is a former American University Professor of Graphic Design, as well as an experienced and award-winning interactive designer, illustrator, and webcomic artist. Ross has a Bachelor of Arts in Graphic Design from American University, and has worked with a wide variety of businesses and organizations over the past ten years. Companies as varied as Edelman Public Relations and Living Social recognize Ross as an expert asking him to speak on topics of design and creativity.</p>
         <div class="about-social">
-            <a href="http://rosscott.tumblr.com/"><span class="fi-social-tumblr"></span></a>
+            <a href="mailto:ross@friendlydesign.co"><span class="icon-email"></span></a>
             <a href="http://rosscott.tumblr.com/"><span class="icon-tumblr"></span></a>
-            <a href="http://rosscott.tumblr.com/"><span class="icon-tumblr"></span></a>
-            <a href="http://rosscott.tumblr.com/"><span class="icon-tumblr"></span></a>
-            <a href="http://rosscott.tumblr.com/"><span class="icon-tumblr"></span></a>
+            <a href="https://dribbble.com/rosscott"><span class="icon-dribbble"></span></a>
+            <a href="http://twitter.com/rosscott"><span class="icon-twitter"></span></a>
+            <a href="http://www.linkedin.com/in/rossnover"><span class="icon-linkedin"></span></a>
         </div>
     </div>
 </div>
@@ -38,11 +37,11 @@
     <div class="abouttext">
         <p><span>Geoff</span> is a creative, multidisciplinary marketing, communications, and design professional. He previously worked as the Assistant Dean of Marketing and Strategy for the American University Washington Semester Program, earned a Bachelor of Fine Arts from the University of Michigan, and a Master of Business Administration from American University.</p>
         <div class="about-social">
-            <a href="http://rosscott.tumblr.com/"><span class="fi-social-tumblr"></span></a>
-            <a href="http://rosscott.tumblr.com/"><span class="icon-tumblr"></span></a>
-            <a href="http://rosscott.tumblr.com/"><span class="icon-tumblr"></span></a>
-            <a href="http://rosscott.tumblr.com/"><span class="icon-tumblr"></span></a>
-            <a href="http://rosscott.tumblr.com/"><span class="icon-tumblr"></span></a>
+            <a href="mailto:geoff@friendlydesign.co"><span class="icon-email"></span></a>
+            <a href="http://blog.gff.mx"><span class="icon-tumblr"></span></a>
+            <a href="https://twitter.com/gffmx"><span class="icon-twitter"></span></a>
+            <a href="http://www.linkedin.com/in/geoffmax/"><span class="icon-linkedin"></span></a>
+            <a href="http://dribbble.com/gffmx"><span class="icon-dribbble"></span></a>
         </div>
     </div>
 </div>
@@ -53,11 +52,11 @@
     <div class="abouttext">
         <p><span>Cristina</span> Cristina is a graphic designer and illustrator with a passion for all things interactive. She has previously worked as a designer for the Investigative Reporting Workshop and earned a Bachelor's of Fine Arts from American University. When the work day ends you can catch her loitering at the local dog park or scouring thrift stores for Hawaiian shirts.</p>
         <div class="about-social">
-            <a href="http://rosscott.tumblr.com/"><span class="fi-social-tumblr"></span></a>
-            <a href="http://rosscott.tumblr.com/"><span class="icon-tumblr"></span></a>
-            <a href="http://rosscott.tumblr.com/"><span class="icon-tumblr"></span></a>
-            <a href="http://rosscott.tumblr.com/"><span class="icon-tumblr"></span></a>
-            <a href="http://rosscott.tumblr.com/"><span class="icon-tumblr"></span></a>
+            <a href="mailto:cristina@friendlydesign.co"><span class="icon-email"></span></a>
+            <a href="#"><span class="icon-tumblr"></span></a>
+            <a href="#"><span class="icon-twitter"></span></a>
+            <a href="#"><span class="icon-linkedin"></span></a>
+            <a href="#"><span class="icon-dribbble"></span></a>
         </div>
     </div>
 </div>
